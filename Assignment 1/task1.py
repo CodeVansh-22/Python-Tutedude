fff3e0c76a06fd32c71f4cb899002e52a22395bd
CodeVansh-22 <vanshchauhand@gmail.com>
2025-09-01 22:32:18 +0530

    Module 2 Assignment

---
+++ Assignment 1/task1.py
@@ -0,0 +1,12 @@
+num1=input("Enter A First Number:")
+num2=input("Enter A Second Number:")
+num1=int(num1)
+num2=int(num2)
+add = num1+num2
+print("Addition Of",num1," and",num2," is",":",add)
+sub = num1-num2
+print("Subtraction Of",num1," and",num2," is",":",sub)
+mul = num1*num2
+print("Multiplication Of",num1," and",num2," is",":",mul)
+div = num1/num2
+print("Division Of",num1," and",num2," is",":",div)
