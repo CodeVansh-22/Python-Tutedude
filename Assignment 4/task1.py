26dff101d0bcb11df3b4d8f907792652d002a8c7
CodeVansh-22 <vanshchauhand@gmail.com>
2025-09-07 18:20:23 +0530

    Module 5 Assignment

---
+++ Assignment 4/task1.py
@@ -0,0 +1,11 @@
+filename = 'sample.txt'
+
+try:
+    with open(filename, 'r') as file:
+        print("Reading The File:")
+        line_number = 1
+        for line in file:
+            print(f"Line {line_number} : {line.strip()}")
+            line_number += 1
+except FileNotFoundError:
+    print(f"Error: The file '{filename}' does not exist.")
