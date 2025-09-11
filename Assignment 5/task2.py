62ab1fbb3234f18bbd2b82e74892971af70e9b12
CodeVansh-22 <vanshchauhand@gmail.com>
2025-09-11 22:21:09 +0530

    Module 6 Assignment

---
+++ Assignment 5/task2.py
@@ -0,0 +1,6 @@
+numbers = list(range(1,11))
+first_five = numbers[:5]
+reversed_list=numbers[4::-1]
+print("Original List:",numbers)
+print("Extracted First Five Elements:",first_five)
+print("Reverse Extracted Elements:",reversed_list)
